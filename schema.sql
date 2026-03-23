4039ff947f0c07c219565eb25f478cd649993132
0xHori <pelasik@yandex.ru>
2026-03-23 22:41:06 +0300

    feat: Создана схема БД

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_name TEXT NOT NULL,
+    task_description TEXT,
+    status TEXT NOT NULL CHECK(status IN ('open', 'todo', 'in_progress', 'done')),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
